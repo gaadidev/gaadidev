e1d9cc7823b4c1539ca7803c12f93b1343dbe988
deepesh <deepeshsharma2011@gmail.com>
2020-07-14 18:37:34 +0530

    https://console.firebase.google.com/u/0/project/evaluator-63e08/crashlytics/app/android:com.gaadi.inspection/issues/b5e6edadef8b5b3daf5d7f70624fcec8?time=last-thirty-days&sessionId=5F070287009800012D2BA9FD91115DA7_DNE_0_v2

--- Neon/src/main/java/com/gaadi/neon/adapter/ImagesFoldersAdapter.java
+++ Neon/src/main/java/com/gaadi/neon/adapter/ImagesFoldersAdapter.java
@@ -14,6 +14,7 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.gaadi.neon.activity.gallery.GridFilesActivity;
 import com.gaadi.neon.activity.gallery.HorizontalFilesActivity;
+import com.gaadi.neon.enumerations.GalleryType;
 import com.gaadi.neon.model.BucketModel;
 import com.gaadi.neon.util.Constants;
 import com.gaadi.neon.util.NeonImagesHandler;
@@ -93,7 +94,11 @@ public class ImagesFoldersAdapter extends BaseAdapter {
             @Override
             public void onClick(View v) {
                 Intent filesIntent;
-                switch (NeonImagesHandler.getSingleonInstance().getGalleryParam().getGalleryViewType()){
+                GalleryType type = GalleryType.Grid_Structure;
+                if(null != NeonImagesHandler.getSingletonInstance().getGalleryParam()){
+                    type = NeonImagesHandler.getSingletonInstance().getGalleryParam().getGalleryViewType();
+                }
+                switch (type){
                     case Grid_Structure:
                         filesIntent = new Intent(context, GridFilesActivity.class);
                         break;
