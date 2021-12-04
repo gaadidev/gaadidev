35fe2bffa7e416cae0090a3256ba5e5a5b4f4f3a
ashish <ashish.kumar@girnarsoft.com>
2021-12-04 07:37:09 +0530

    issue resolved

--- Neon/src/main/java/com/gaadi/neon/util/FileUtils.java
+++ Neon/src/main/java/com/gaadi/neon/util/FileUtils.java
@@ -27,11 +27,11 @@ public class FileUtils {
 
     public static Uri createDocFileName(Context context, String path, String displayName){
 
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return createDocFileAboveQ(context, path,  displayName, Constants.PIC_MIME_TYPE);
+        } else {
             File file = createDocFileLegacy(path, displayName);
             return Uri.fromFile(file);
-        } else {
-            return createDocFileAbovePie(context, path,  displayName, Constants.PIC_MIME_TYPE);
         }
     }
     private static File createDocFileLegacy(String path, String displayName){
@@ -46,7 +46,7 @@ public class FileUtils {
     }
 
     @TargetApi(Build.VERSION_CODES.Q)
-    private static Uri createDocFileAbovePie(Context context, String path, String displayName, String docType){
+    private static Uri createDocFileAboveQ(Context context, String path, String displayName, String docType){
         ContentResolver resolver = context.getContentResolver();
         ContentValues contentValues = new ContentValues();
         contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, path);
@@ -55,62 +55,19 @@ public class FileUtils {
         return resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
     }
     public static String getPath (Context context, Uri uri) {
-        try {
-            context.grantUriPermission(context.getPackageName(), uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            String[] projection = {MediaStore.MediaColumns.DATA};
-            Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        } catch (Exception e){
-            return "";
-        }
-
-    }
-
-    public static File getImageFilePath(Context context, Uri uri) throws IOException {
-        context.grantUriPermission(context.getPackageName(), uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        String fileName = getFileName(context, uri);
-        File file = File.createTempFile((fileName != null && !fileName.isEmpty()) ? fileName : "" + UUID.randomUUID().toString(), ".jpg");
-        file.deleteOnExit();
-        InputStream inputStream = context.getContentResolver().openInputStream(uri);
-
-        FileOutputStream out = new FileOutputStream(file);
-        if (inputStream != null) {
-            copyStream(inputStream, out);
-            out.close();
-        }
-        return file;
-    }
-
-    public static String getFileName(Context context, Uri uri) {
-        String result = "";
-        if (uri.getScheme() != null && uri.getScheme().contains("content")) {
-            Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             try {
-                if (cursor != null && cursor.moveToFirst()) {
-                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                }
-            } finally {
-                if (cursor != null)
-                    cursor.close();
+                context.grantUriPermission(context.getPackageName(), uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                String[] projection = {MediaStore.MediaColumns.DATA};
+                Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            } catch (Exception e){
+                return "";
             }
-        }
-        if (result == null) {
-            result = uri.getPath();
-            int cut = result.lastIndexOf('/');
-            if (cut != -1) {
-                result = result.substring(cut + 1);
-            }
-        }
-        return result;
-    }
-
-    public static void copyStream(InputStream in, OutputStream out) throws IOException {
-        byte[] buffer = new byte[4096];
-        int read;
-        while ((read = in.read(buffer)) != -1) {
-            out.write(buffer, 0, read);
+        } else {
+            return uri.getPath();
         }
     }
 }
